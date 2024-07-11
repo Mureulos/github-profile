a7040b90fbda22d7031b983815de89e2ad308fce
mureulos <murilodesantana05@gmail.com>
2024-07-10 23:42:51 -0300

    Criação e estilização dos componentes de repositórios

--- src/app/shared/components/respositories/respositories.component.ts
+++ src/app/shared/components/respositories/respositories.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { ReposType } from '../../interfaces/type';
 
 @Component({
   selector: 'app-respositories',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './respositories.component.html',
   styleUrl: './respositories.component.scss'
 })
 export class RespositoriesComponent {
-
+  @Input() apiData: ReposType[] = []
 }
