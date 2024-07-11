b69223940bba8f7a96e7c4db22e23c34fdaac5a1
mureulos <murilodesantana05@gmail.com>
2024-07-10 23:42:21 -0300

    Criação e estilização dos data cards

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataCardComponent } from '../../shared/components/data-card/data-card.component';
+import { DropdownComponent } from '../../shared/components/dropdown/dropdown.component';
+import { RespositoriesComponent } from '../../shared/components/respositories/respositories.component';
+import { EMPTY_REPOS, ProfileType, ReposType } from '../../shared/interfaces/type';
+import { GithubProfileService } from '../../shared/services/github-profile.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [DataCardComponent, DropdownComponent, RespositoriesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  profileData: ProfileType[] = []
+  reposData: ReposType = EMPTY_REPOS
+  constructor(private githubProfileService: GithubProfileService) {}
 
+  ngOnInit(): void {
+    console.log()
+    this.getData('Mureulos')
+  }
+
+  public getData(username: string) {
+    this.githubProfileService.getData(username).subscribe(response => {
+      this.profileData = response
+    })
+  }
+
+  public getRepos(username: string) {
+    this.githubProfileService.getRepos(username).subscribe(response => {
+      this.reposData = response
+    })
+  }
 }
--- src/app/shared/components/data-card/data-card.component.ts
+++ src/app/shared/components/data-card/data-card.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { EMPTY_PROFILE, ProfileType } from '../../interfaces/type';
+import { profile } from 'node:console';
 
 @Component({
   selector: 'app-data-card',
   standalone: true,
   imports: [],
   templateUrl: './data-card.component.html',
-  styleUrl: './data-card.component.scss'
+  styleUrl: './data-card.component.scss',
 })
 export class DataCardComponent {
-
+  @Input() apiData: ProfileType = EMPTY_PROFILE;
 }
