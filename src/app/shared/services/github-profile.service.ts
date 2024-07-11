66e0276960d94dadb2de41a7eb4b23b5ab07d890
mureulos <murilodesantana05@gmail.com>
2024-07-10 23:40:20 -0300

    Funções de fetch e types

--- src/app/shared/services/github-profile.service.ts
+++ src/app/shared/services/github-profile.service.ts
@@ -1,18 +1,22 @@
 //app/shared/services/coffee-list.service.ts
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ProfileType } from '../interfaces/type';
-import { environment } from '../../../environments/environment.development';
+import { ReposType } from '../interfaces/type';
 
 @Injectable({
   providedIn: 'root',
 })
-export class CoffeeListService {
+export class GithubProfileService {
   private apiUrl: string = 'https://api.github.com/users/';
   constructor(private httpClient: HttpClient) {}
 
   getData(username: string): Observable<ProfileType[]> {
     return this.httpClient.get<ProfileType[]>(`${this.apiUrl}${username}`);
+  }
+
+  getRepos(username: string): Observable<ReposType[]> {
+    return this.httpClient.get<ReposType[]>(`${this.apiUrl}${username}/repos`);
   }
 }
